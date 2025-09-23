cc07a1979d8be0b8801388b855ac5425a4f349b4
Sarthak Agarwal <sarthakroxxx@gmail.com>
2025-09-23 21:44:49 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/health", (c) => {
+  return c.json({
+    error: false,
+    message: "Server is running",
+    data: {},
+  });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
