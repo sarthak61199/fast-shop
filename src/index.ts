4f1e74e0897d97351b4e0329e852e10f22c98b2d
Sarthak Agarwal <sarthakroxxx@gmail.com>
2025-09-23 22:32:35 +0530

    added error middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { secureHeaders } from "hono/secure-headers";
 import { logger } from "hono/logger";
+import { secureHeaders } from "hono/secure-headers";
 
+import { HTTPException } from "hono/http-exception";
 import addressController from "./controllers/address.js";
 import authController from "./controllers/auth.js";
 import cartController from "./controllers/cart.js";
@@ -12,7 +13,28 @@ import paymentController from "./controllers/payment.js";
 import productController from "./controllers/product.js";
 import userController from "./controllers/user.js";
 
-const app = new Hono().basePath("/api/v1");
+const app = new Hono().basePath("/api/v1").onError((err, c) => {
+  console.error(err);
+  if (err instanceof HTTPException) {
+    return c.json(
+      {
+        error: true,
+        message: err.message,
+        data: {},
+      },
+      err.status
+    );
+  }
+
+  return c.json(
+    {
+      error: true,
+      message: "Internal server error",
+      data: {},
+    },
+    500
+  );
+});
 
 app.use(logger());
 app.use(cors());
